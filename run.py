32a6d05c597820702835cf1966f2485cc0402310
Adam Wirth <adamowirth@gmail.com>
2020-04-02 16:00:41 -0400

    initial commit. leaving gists for a real repo

---
+++ run.py
@@ -0,0 +1,115 @@
+from math import ceil
+import random
+import argparse
+
+from matplotlib import pyplot as plt
+import numpy as numpy
+
+parser = argparse.ArgumentParser(description='Test batched testing approaches.')
+parser.add_argument('tests', default=100, type=int, nargs='?')
+parser.add_argument('positives', default=19, type=int, nargs='?')
+parser.add_argument('batch', default=5, type=int, nargs='?')
+parser.add_argument('runs', default=1000, type=int, nargs='?')
+
+args = vars(parser.parse_args())
+tests = args['tests']
+positives = args['positives']
+batch = args['batch']
+runs = args['runs']
+
+assert batch >= 0 and tests > 0 and positives > 0, 'what are you doing? stop it!'
+assert batch <= tests, 'batch > tests, oof'
+assert positives <= tests, 'positives > tests, big oof'
+
+def _chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+def chunks(lst, n):
+    return list(_chunks(lst, n))
+
+def hasTrue(lst):
+    return True if True in lst else False
+
+def countPartitions(partition):
+    i = 0
+    for p in partition:
+        if hasTrue(p):
+            i += 1
+    return i
+
+truncate = lambda v: ceil(v * 1000) / 1000
+
+def plot(graph, batches, **kwargs):
+    print(kwargs, kwargs.values())
+    graph.plot(list(kwargs.keys()), list(kwargs.values()), label='Batches ' + str(batches))
+    return graph
+
+def compute(batch):
+    random.seed()
+
+    negatives = tests - positives
+
+    print('tests:\t\t%s' % tests)
+    print('positives:\t%s' % positives)
+    perc = 100 / (tests / positives)
+    perc_rounded = truncate(perc)
+    print('%' + ' positive:\t%s' % perc + '%')
+    print('batch:\t\t%s' % batch)
+
+    top_level = ceil(tests / batch)
+    print('top_level:\t%s' % top_level)
+
+    print('-----')
+
+    best_case = top_level + (ceil(positives / batch) * batch)
+    print('best_case:\t%s' % best_case)
+
+    worst_case = top_level + ((positives if positives < top_level else top_level) * batch)
+    print('worst_case:\t%s' % worst_case)
+
+    print('-----')
+
+    # Time to monte carlo!
+    tested = []
+    for i in range(positives):
+        tested.append(True)
+    for i in range(negatives):
+        tested.append(False)
+
+    def monteCarlo():
+        random.shuffle(tested)
+
+        partitioned = chunks(tested, batch)
+
+        # this many partitions have at least one positive test
+        count = countPartitions(partitioned)
+
+        return count
+
+    print('Running %s scenarios...' % runs)
+    total = 0
+    for i in range(runs):
+        total += monteCarlo() * batch
+
+    average_run = total / runs
+    print('average_run:\t%s' % average_run)
+    average_case = top_level + average_run
+    print('average_case:\t%s' % average_case)
+    assert average_case <= worst_case and average_case >= best_case, 'this should be impossible'
+    diff = truncate(tests - average_case)
+    if diff > 0:
+        print('this setup would SAVE ~%s extra tests' % diff)
+    else:
+        print('this setup would LOST ~%s more tests' % diff)
+
+    return { 'best_case': best_case, 'worst_case': worst_case, 'average_case': average_case }
+
+if __name__ == '__main__':
+    print('test_shortage.py running')
+    for b in range(batch - 4, batch + 15):
+        plot(plt, b, **compute(b))
+    plt.legend()
+    plt.suptitle(str(args))
+    plt.show()
